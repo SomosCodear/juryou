68d159f61d51907b0a18400c318ba64b539caad7
Agustin Carrasco <asermax@gmail.com>
2020-04-19 19:12:09 -0300

    chore(package): make requirements file the source of truth

--- setup.py
+++ setup.py
@@ -3,9 +3,25 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+# get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as readme:
+    long_description = readme.read()
+
+# get install requirements from requirements.txt file
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as requirements:
+    install_requires = [
+        line.strip()
+        for line in requirements.readlines()
+        if not line.startswith('#')
+    ]
+
+# get test requirements from requirements-test.txt file
+with open(path.join(here, 'requirements-test.txt'), encoding='utf-8') as requirements:
+    tests_require = [
+        line.strip()
+        for line in requirements.readlines()
+        if not line.startswith('#')
+    ]
 
 setup(
     name='juryou',
@@ -18,16 +34,9 @@ setup(
     author_email='desarrollo@codear.org',
     packages=find_packages(),
     python_requires='>=3.7',
-    install_requires=[
-        'py3afipws',
-        'httplib2',
-        'py3simplesoap',
-        'pyopenssl',
-        'weasyprint',
-        'python-barcode',
-        'jinja2',
-    ],
+    install_requires=install_requires,
     test_suite='juryou.tests',
+    tests_require=tests_require,
     entry_points={
         'console_scripts': [
             'juryou=juryou.cli:main',
