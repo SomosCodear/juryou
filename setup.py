dd2d253409f400af3636d20caef92289f5fd2101
Agustin Carrasco <asermax@gmail.com>
2020-05-19 01:56:50 -0300

    chore(setup): bump version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open(path.join(here, 'requirements-test.txt'), encoding='utf-8') as require
 
 setup(
     name='juryou',
-    version='0.3.2',
+    version='0.3.3',
     description='Simple receipt generation library',
     long_description=long_description,
     long_description_content_type='text/markdown',
